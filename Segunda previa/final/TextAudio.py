cae5955540d81314a21ddd7ee7a6fb5e6d1c722f
JuanDa15 <59321452+JuanDa15@users.noreply.github.com>
2020-11-14 14:46:42 -0500

    cambio final

---
+++ Segunda previa/final/TextAudio.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Di algo.')
+    audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio)
+        print('lo que dijiste fue: {}'.format(text))
+    except:
+        print('lo siento no puedo entendderlo')
